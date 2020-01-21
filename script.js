8e0b03cb928d10a41fe510e9a1f973f57a9e7ba5
Yura Alekseev <yuraalekseev00@gmail.com>
2020-01-21 16:29:05 +0200

    added some fixes

--- script.js
+++ script.js
@@ -12,10 +12,12 @@ let appData = {
 
 let firstQuestion1 = prompt('Введите обязательную статью расходов в этом месяце', '');
 let secondQuestion1 = prompt('Во сколько обойдется?', '');
-appData.expenses.firstQuestion1 = secondQuestion1;
+appData.expenses[firstQuestion1] = secondQuestion1;
 
 let firstQuestion2 = prompt('Введите обязательную статью расходов в этом месяце', '');
 let secondQuestion2 = prompt('Во сколько обойдется?', '');
-appData.expenses.firstQuestion2 = secondQuestion2;
+appData.expenses[firstQuestion2] = secondQuestion2;
+
+console.log(appData.expenses);
 
 alert('Ваш бюджет на один день: ' + appData.budget / 30);
